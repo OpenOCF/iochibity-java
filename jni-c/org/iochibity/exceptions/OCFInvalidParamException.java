329d0015b23bd743b4ed563744ef1200df629ea1
Gregg Reynolds <reynolds-gregg@norc.org>
2016-12-03 03:33:17 -0600

    remove unneeded exception
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- jni-c/org/iochibity/exceptions/OCFInvalidParamException.java
+++
@@ -1,7 +0,0 @@
-package org.iochibity.exceptions;
-
-public class OCFInvalidParamException extends RuntimeException {
-    public OCFInvalidParamException(String message) {
-        super(message);
-    }
-}
